d16cb50879153f12f950c2da19445f308ede2996
Martin Palecek <martin.palecek@ketry.net>
2018-03-08 23:11:27 +0100

    NOISSUE Rudimentary handling of colony seizing

--- mapa_zobraz_galaxy.php.user.js
+++ mapa_zobraz_galaxy.php.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         mapa_zobraz_galaxy.php
 // @namespace    http://stargate-dm.cz/
-// @version      0.4
+// @version      0.5
 // @description  Utils for mapa_zobraz_galaxy.php
 // @author       on/off
 // @match        http://stargate-dm.cz/mapa_zobraz_galaxy.php*
@@ -23,6 +23,16 @@
             var type = qt ? XPathResult.FIRST_ORDERED_NODE_TYPE: XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE;
             var ret = document.evaluate(query, object, null, type, null);
             return (qt ? ret.singleNodeValue : ret);
+        };
+
+        this.getParameterByName = function (name, url) {
+            if (!url) url = window.location.href;
+            name = name.replace(/[\[\]]/g, "\\$&");
+            var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, " "));
         };
 
         this.sector_table_update = function() {
@@ -68,17 +78,16 @@
             }
         };
 
-
         this.colony_table_update = function() {
             var all = tools.xpath('//*[@id="all"]',null,true);
             var rect = document.getElementById('report_button').getBoundingClientRect();
             var free_colonies = document.createElement("div");
             free_colonies.setAttribute("id", "free_colonies");
-            free_colonies.setAttribute("style","position: absolute; width: 150px; top: " +(rect.top-4)+ "px; left: " +(rect.left+110)+ "px; z-index: 99; font-size: 75%; text-align: left; background-color: rgba(0,0,0,0.5); padding: 1em; border: 1px solid black;");
+            free_colonies.setAttribute("style","position: absolute; width: 200px; top: " +(rect.top-4)+ "px; left: " +(rect.left+110)+ "px; z-index: 99; font-size: 75%; text-align: left; background-color: rgba(0,0,0,0.5); padding: 1em; border: 1px solid black;");
             all.parentNode.insertBefore(free_colonies, all);
             var sectors = this.xpath('//*[@id="map"]/area');
-            var found = false;
-            for (var s = 0; s < sectors.snapshotLength && !found; ++s) {
+            var found = 0;
+            for (var s = 0; s < sectors.snapshotLength && found < 30; ++s) {
                 var this_sector = sectors.snapshotItem(s);
                 var href = this_sector.getAttribute('href');
                 var title = this_sector.getAttribute('title');
@@ -92,14 +101,39 @@
                         $.each(colonies, function(free_colonies, k, v) {
                             var this_colony = v.parentNode;
                             if (-1 != this_colony.innerHTML.indexOf('Majitel: neobsazeno') && -1 != this_colony.innerHTML.indexOf('Velikost: velká')) {
+                                found++;
                                 var this_link = v.getAttribute('href');
-                                free_colonies.innerHTML += '<div><a href="' +this_link+ '">Kolonie</a></div>';
-                                found = true;
+                                var this_name = this_colony.innerHTML.slice(this_colony.innerHTML.indexOf('Umístění: ')+10);
+                                this_name = this_name.slice(0,this_name.indexOf('\n'));
+                                this_name = this_name.slice(this_name.indexOf('-')+2);
+
+                                var antihack = tools.xpath('//*[@id="content-in"]/center/form/p/input', null, true).value;
+                                console.log('Antihack: ' +antihack);
+                                var galaxy      = tools.getParameterByName('galaxy',      this_link);
+                                var typ_ast     = tools.getParameterByName('typ_ast',     this_link);
+                                var asteroid_os = tools.getParameterByName('asteroid_os', this_link);
+                                console.log('Galaxy: ' +galaxy+ ', typ_ast: ' +typ_ast+ ', asteroid_os: ' +asteroid_os);
+                                var post_body = 'antihack=' +antihack+ '&name=' +asteroid_os+ '&galaxy=' +galaxy+ '&sektor=1&sektorC=&typ_ast=' +typ_ast+ '&asteroid_os=' +asteroid_os+ '&konvoj=+Vy%C5%A1li+';
+                                console.log(post_body);
+
+                                free_colonies.innerHTML += '<div><input type="submit" class="submit" style="float: right;" value="Obsadit" onclick="tools.seize_colony(\'' +post_body+ '\')" /><a href="' +this_link+ '">' +this_name+ '</a><hr style="clear: both; display: block; visibility: hidden;" /></div>';
                             }
                         }.bind(this, free_colonies));
                     }
                 });
             }
+        };
+
+        this.seize_colony = function(post_body) {
+            console.log('seize_colony: ' +post_body);
+            $.ajax({
+                type: 'POST',
+                url:  '/kolonie.php',
+                data:  post_body,
+                success: function(data, status) {
+                    alert('Vysláno');
+                }
+            });
         };
     }
 
